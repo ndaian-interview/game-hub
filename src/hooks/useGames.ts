c311c500821f9765d883e458aa3d0b0e82e0a965
Nicolae Daian <tech.ndaian@gmail.com>
2025-12-06 12:10:35 -0800

    Show loading skeletons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -24,13 +24,19 @@ interface GamesRequestResponse {
 const useGames = () => {
   const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(true);
 
   useEffect(() => {
     const controller = new AbortController();
+    setLoading(true);
     apiClient
       .get<GamesRequestResponse>("/games", { signal: controller.signal })
-      .then((res) => setGames(res.data.results))
+      .then((res) => {
+        setGames(res.data.results);
+        setLoading(false);
+      })
       .catch((err) => {
+        setLoading(false);
         if (err instanceof CanceledError) return;
         setError(err.message);
       });
@@ -38,7 +44,7 @@ const useGames = () => {
     return () => controller.abort();
   }, []);
 
-  return { games, error };
+  return { games, error, isLoading };
 };
 
 export default useGames;
