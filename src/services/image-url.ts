4eb202e66e48153c1237d77168535bcdc275983a
Nicolae Daian <tech.ndaian@gmail.com>
2025-12-06 20:11:01 -0800

    Handle games with no image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,5 +1,7 @@
+import noImage from "../assets/Image Placeholder/no-image-placeholder.webp";
+
 const getCroppedImageUrl = (url: string): string => {
-  if (!url) return "";
+  if (!url) return noImage;
   const target = "media/";
   const croppedUrl = url.replace(target, `${target}crop/600/400/`);
   return croppedUrl;
