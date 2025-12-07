45af59df4a1e5bd8937aecc179e82c4f69f42d90
Nicolae Daian <tech.ndaian@gmail.com>
2025-12-06 20:05:06 -0800

    Sort the games

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,4 +1,5 @@
 const getCroppedImageUrl = (url: string): string => {
+  if (!url) return "";
   const target = "media/";
   const croppedUrl = url.replace(target, `${target}crop/600/400/`);
   return croppedUrl;
